5eef255cd5426b7d2bee8cc433fcb498d29f6526
Ravi <ravi.chandak901@gmail.com>
2015-07-18 07:38:56 +0530

    Latest

--- InterfaceProj.js
+++ InterfaceProj.js
@@ -24,16 +24,21 @@ if (Meteor.isServer) {
 */
 Tokens = new Mongo.Collection('tokens');//Meteor.userId()==token ;
 Todos = new Mongo.Collection('todos');//Metoer.userId() == todo == priority 
-Calendars = new Mongo.Collection('calendars');//
+//Calendars = new Mongo.Collection('calendars');//
 
 
 if (Meteor.isClient) {
   
    Template.main.created = function(){
         if(Session.get('token1')===undefined){
+          var controller = Iron.controller();
+
+    // reactively return the value of postId
+    var token =  controller.state.get('token');
+    Session.set('token1' , token);
    //  window.location.assign('https://ivle.nus.edu.sg/api/login/?apikey=0J5cKRFGUQASyFHiJ07v4&url=http://localhost:3000');
-          Session.set('token1' , window.location.href.split('=')[1]);
-          console.log(Session.get('token1'));
+         // Session.set('token1' , window.location.href.split('=')[1]);
+          //console.log(Session.get('token1'));
           }
     };
   
@@ -49,7 +54,10 @@ if (Meteor.isClient) {
     Meteor.setInterval(function(){
       Session.set('time' , new Date);
     } , 5000);
-    Meteor.subscribe('calendars');
+    //Meteor.subscribe('calendars');
+    Meteor.subscribe('calendar', function () {
+  Session.set('superCalendarReady', true);
+});
     Meteor.subscribe('tokens');
     Meteor.subscribe('todos');
     
@@ -101,11 +109,6 @@ Modal.allowMultiple = true
     weather2 : function(){
       var out="" ; 
       var currWeather = Meteor.call('weather' , function(err,res){
-     // Meteor.call('weather' , function(err,response)
-      //{
-       // currWeather = response.weather.description;
-        //Session.set('a' , currWeather);
-     //   );    
       console.log(res);
       out = res.weather[0].description;
       out2 = res.weather[0].icon;
@@ -124,11 +127,17 @@ Modal.allowMultiple = true
 
   Template.login.events({
     'click button' : function(event){
-      window.location.assign('https://ivle.nus.edu.sg/api/login/?apikey=0J5cKRFGUQASyFHiJ07v4&url=http://localhost:3000/login');
-//      Router.render('https://ivle.nus.edu.sg/api/login/?apikey=0J5cKRFGUQASyFHiJ07v4&url=https://localhost:3000');
-       var token = window.location.href.split('=')[1];
-       Session.set('token' , token)
-       console.log(Session.get('token'));
+     window.location.assign('https://ivle.nus.edu.sg/api/login/?apikey=0J5cKRFGUQASyFHiJ07v4&url=http://localhost:3000');
+ //    Router.render('https://ivle.nus.edu.sg/api/login/?apikey=0J5cKRFGUQASyFHiJ07v4&url=https://localhost:3000');
+       
+       //var token = window.location.href.split('=')[1];
+       //var t = Meteor.call('getToken' , function(err,token){
+        //console.log(token);
+        //Session.set('token' , token)
+       //console.log(Session.get('token'));
+       //})
+      //Session.set('token' , token)
+     // console.log(Session.get('token'));
     }
   });
 
@@ -141,114 +150,104 @@ Modal.allowMultiple = true
 
               console.log('click successful');
 }*/
-Template.myTemplate.rendered = function(){
-  $(window).load(function(){
-        $('#myModal').modal('hide');
-    });
-}
-
-Template.editEvent.events({
-  'click .save':function(evt,tmpl){
-    console.log(tmpl.find('.title').value);    
-    Meteor.call('addCalendarEvents' , Session.get('date'),tmpl.find('.title').value);
-
-    tmpl.find('.title').value = "";
-    Session.set('editing_calevent',null);
-    Session.set('showEditEvent',false);
-    $('#EditEventModal').modal("hide");
+Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Todos.update(this._id, {$set: {checked: ! this.checked}});
     },
-  'click .close':function(evt,tmpl){
-    Session.set('editing_calevent',null);
-    Session.set('showEditEvent',false);
-    $('#EditEventModal').modal("hide");
-  } ,
-  'click .remove':function(evt,tmpl){
-    //removeCalEvent(Session.get('editing_calevent'));
-    Session.set('editing_calevent',null);
-    Session.set('showEditEvent',false);
-    $('#EditEventModal').modal("hide");
-  }
-})
-Template.myTemplate.helpers({
-        calendarOptions:function() {
-            // Standard fullcalendar options
-            return {
-            height:40,
-            contentHeight : 400,
-            aspectRatio : 1,
-            fixedWeekCount : false,
-            hiddenDays: [],
-            slotDuration: '01:00:00',
-            minTime: '08:00:00',
-            maxTime: '19:00:00',
-            lang: 'en',
-            // Function providing events reactive computation for fullcalendar plugin
-            events: function(start, end, timezone, callback) {
-                //console.log(start);
-                //console.log(end);
-                //console.log(timezone);
-                var events = [];
-                // Get only events from one document of the Calendars collection
-                // events is a field of the Calendars collection document
-                var calendar = Calendars.findOne(
-                    { "_id":"myCalendarId" },
-                    { "fields": { 'events': 1 } }
-                );
-                // events need to be an array of subDocuments:
-                // each event field named as fullcalendar Event Object property is automatically used by fullcalendar
-                if (calendar && calendar.events) {
-                    calendar.events.forEach(function (event) {
-                        eventDetails = {};
-                        for(key in event)
-                            eventDetails[key] = event[key];
-                        events.push(eventDetails);
-                    });
-                }
-                callback(events);
-            },
-            dayClick : function(date,jsEvent,view){
-              $('#EditEventModal').modal('show');
-              Session.set('date',date.format());
-              //Meteor.call('addCalendarEvents' , date,  Session.get('content') );
-              console.log(Calendars.find().fetch());
-
-              console.log('click successful');
-
-            },
-            // Optional: id of the calendar
-            id: "calendar1",
-            // Optional: Additional classes to apply to the calendar
-            addedClasses: "col-md-8",
-            // Optional: Additional functions to apply after each reactive events computation
-            autoruns: [
-                function () {
-                    console.log("user defined autorun function executed!");
-                }
-            ]
-        };
+    "click .delete": function () {
+      Todos.remove(this._id);
+    }
+  });
+ Template.todo.helpers({
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Todos.find({checked: {$ne: true}, owner : Meteor.userId()}, {sort: {createdAt: -1,priority : -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Todos.find({owner : Meteor.userId()}, {sort: { priority : -1 , createdAt: -1 }});
       }
-    });
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Todos.find({checked: {$ne: true} ,owner : Meteor.userId()} ).count();
+    }
+  });
+
+
+SuperCalendar.rendered = function () {
+var self = this;
+
+    self.autorun(function () {
+      if (! Session.get('superCalendarReady', true) ||
+          typeof Calendar === 'undefined') {
+        return;
+      }
+      var entries = Calendar.find({userId : Meteor.userId()}).fetch();
+      var $calendar = $('#calendar');
+
+      $calendar.html('').fullCalendar({
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay'
+        },
+        editable: false,
+        events: entries,
+        eventRender: function (event, element) {
+          $(element).attr('id', event._id);
+        },
+        dayClick: function (date, flag, e, view) {
+          return SuperCalendar.events.onDayClick.call(this, e, self, {
+            date: date,
+            view: view
+          });
+        },
+        eventClick: function (date, e, view) {
+          return SuperCalendar.events.onEventClick.call(this, e, self, {
+            date: date,
+            view: view
+          });
+        }
+      });
+    });  
+// Do something custom.
+};
+
+  Template.todo.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+      var text = event.target.text.value;
+      var priority = event.target.priority.value;
+      console.log(text +" " + priority);
+
+       Meteor.call('todoInsert' , text , priority);
+      //Todos.insert({
+        //text: text,
+       // createdAt: new Date(),            // current time
+       // owner: Meteor.userId(),           // _id of logged in user
+       // username: Meteor.user().username  // username of logged in user
+     // });
+
+      // Clear form
+      event.target.text.value = "";
+      event.target.priority.value="1";
+
+      // Prevent default form submit
+      return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
     ////////
-
-
-
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    Meteor.publish('calendar', function () {
-  return Calendar.find();
-});
-    Meteor.publish('todos' , function(){
-      return Todos.find();
-    });
-    Meteor.publish('tokens' , function(){
-      return Tokens.find();
-    });
-    
-    Meteor.methods({
-      weather : function(){
-            
+Meteor.methods({
+    weather : function(){        
 //     var url = "http://api.openweathermap.org/data/2.5/weather?q=singapore&APPID=860925b0e2933e51a216953566d0964d";
   //  var res = "";
     var t = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=singapore&APPID=860925b0e2933e51a216953566d0964d").data;
@@ -260,13 +259,45 @@ if (Meteor.isServer) {
     return t;
   },
     addCalendarEvents : function(date , content){
-      Calendars.insert({userId : Meteor.userId , date : date , content : content})
+      Calendars.insert({userId : Meteor.userId() , date : date , content : content})
     },
     modules : function(){
       var t = HTTP.get('https://ivle.nus.edu.sg/api/Lapi.svc/Modules?APIKey=0J5cKRFGUQASyFHiJ07v4&AuthToken=4DD98A4C7FF723C0FBFDE2965B5ED3EBF4F68AF8D58E3E043A9BD9B076FD22B0292B01595C97115352E3FED29546ED4FA0A9988A100C41A5FCCDA23EE7A0F83CBCE99EBB6F5828AA5F7B49D908139232B955BF4471964B82A117A0DF718C05EB5BA6ED9484FE18DEA814D1C2A6CEEB5AE226727F2B5C87F27F5C8ED74DE8B405CC29EDAEE89784A14ECFCD51E350393F61CB8AA42F17CBBB13ADFF8ABF5CF3A7CE75C1DE79882D695BAC4F9625FF431D822404C8ABD9932512F96CA8C726534211A5CF0C0476844C6AD82F8F46FE26D4A9C09B6C94D500AEF41E33FF8D4A488B29E6B57F9BD9E04CE6A947CD5BD10036');
       return t; 
-    }
+    },
+    getToken : function(){
+      var token = HTTP.get('https://ivle.nus.edu.sg/api/login/?apikey=0J5cKRFGUQASyFHiJ07v4&url=https://localhost:3000');
+      return token;
+    },
+
+    todoInsert : function(text,priority){
+       Todos.insert({
+        text: text,
+        priority : priority,
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
+      });
+    } 
+  });
+
+
+
+
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    Meteor.publish('calendar', function () {
+     return Calendar.find();
     });
+    Meteor.publish('todos' , function(){
+      return Todos.find();
+    });
+    Meteor.publish('tokens' , function(){
+      return Tokens.find();
+    });
+    
+    
     // code to run on server at startup
   });
 }
